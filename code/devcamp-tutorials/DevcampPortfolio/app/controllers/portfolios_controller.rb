848cbbd0e69491321543a5f489f8cf7335e12317
Lyndon Prado <lyndonprado1@gmail.com>
2020-01-28 15:35:49 +1100

    integrated index action for portfolio items

--- code/devcamp-tutorials/DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ code/devcamp-tutorials/DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,2 +1,30 @@
 class PortfoliosController < ApplicationController
+	def index
+		@portfolio_items = Portfolio.all
+    @portfolios = Portfolio.all
+    #render html: '<div> html goes here </div>'.html_safe
+	end
+
+  def list
+    @portfolio_items = Portfolio.all
+  end
+  
+  def show
+    #render html: '<div> html goes here </div>'.html_safe
+    # @portfolio = portfolio_items.find(:all)
+    
+    # redirect_to action: "../views/index"
+   # render html: '../views/portfolios/index.html.erb'}.html_safe
+  end
+
+  def new 
+    @portfolio = Portfolio.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
 end
--- code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
+++ code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  # resources :portfolios
+  # get 'portfolios/index'
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  # get 'portfolios/index'
   resources :blogs
+  get 'portfolios/index' => 'portfolios#index'
+  resources :portfolios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
