164f99296e63eb7f322a061b7a7f9c71e1903399
Lyndon Prado <lyndonprado1@gmail.com>
2020-01-31 13:48:37 +1100

    Customized portfolio routes

--- code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
+++ code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  # resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # get 'portfolios/index'
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  #get 'pages/about'
+  #get 'pages/contact'
   # get 'portfolios/index'
   resources :blogs
-  get 'portfolios/index' => 'portfolios#index'
-  resources :portfolios
+  #get 'portfolios/index' => 'portfolios#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
+
+
+
