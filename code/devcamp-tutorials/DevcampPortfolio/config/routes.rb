87b453780085efe1771abe08b553a9b4d8c7f063
Lyndon Prado <lyndonprado1@gmail.com>
2020-02-07 11:49:39 +1100

    got the toggle status showing and working

--- code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
+++ code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   #get 'pages/about'
   #get 'pages/contact'
   # get 'portfolios/index'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   #get 'portfolios/index' => 'portfolios#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
