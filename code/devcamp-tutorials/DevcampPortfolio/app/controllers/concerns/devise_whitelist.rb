16e0bf35b500ca5d7535ff670cb6ad699dd486e7
Lyndon Prado <lyndonprado1@gmail.com>
2020-02-17 15:46:10 +1100

    Adding session tracker for dynamic source link

--- code/devcamp-tutorials/DevcampPortfolio/app/controllers/concerns/devise_whitelist.rb
+++ code/devcamp-tutorials/DevcampPortfolio/app/controllers/concerns/devise_whitelist.rb
@@ -2,7 +2,7 @@ module DeviseWhitelist
 	extend ActiveSupport::Concern
 	
 	included do 
-		before_filter :configure_permitted_parameters, if: :devise_controller?
+		before_filter :authenticate_user!,:configure_permitted_parameters, if: :devise_controller?
 	
 		protected
 
@@ -13,6 +13,13 @@ module DeviseWhitelist
 
 			devise_parameter_sanitizer.for(:sign_up) << :name
 			devise_parameter_sanitizer.for(:account_update) << :name
+		end
+
+		before_action :set_source
+
+		def set_source
+			puts session[:source].inspect
+			session[:source] = params[:q] if params[:q]
 		end
 	end
 end
