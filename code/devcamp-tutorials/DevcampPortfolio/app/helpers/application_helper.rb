2a37d9b374316ac0cc2871a2e4e9cbce34e8d9bf
Lyndon Prado <lyndonprado1@gmail.com>
2020-02-20 17:10:53 +1100

    Updated master layout styles along with helper methods

--- code/devcamp-tutorials/DevcampPortfolio/app/helpers/application_helper.rb
+++ code/devcamp-tutorials/DevcampPortfolio/app/helpers/application_helper.rb
@@ -3,18 +3,18 @@ module ApplicationHelper
 		content_tag(:div, "My content",class: "my-class")
 	end
 
-	def login_helper
+	def login_helper style
 		if current_user.is_a?(GuestUser) 
-      		(link_to "Register", new_user_registration_path) +
-      		"<br>".html_safe +
-  			(link_to "Login", new_user_session_path)
+      		(link_to "Register", new_user_registration_path, class: style) +
+      		" ".html_safe +
+  			(link_to "Login", new_user_session_path, class: style)
     	elsif current_user.is_a?(User)
-      		link_to "Logout", destroy_user_session_path,method: :delete 
+      		link_to "Logout", destroy_user_session_path,method: :delete, class: style 
       	else
       		current_user = GuestUser.new 
-      		(link_to "Register", new_user_registration_path) +
-      		"<br>".html_safe +
-  			(link_to "Login", new_user_session_path)
+      		(link_to "Register", new_user_registration_path, class: style) +
+      		" ".html_safe +
+  			(link_to "Login", new_user_session_path, class: style)
     	end 
 	end
 
