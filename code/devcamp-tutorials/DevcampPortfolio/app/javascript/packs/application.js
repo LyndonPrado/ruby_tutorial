d5bc69e892d9467364475e373b8adc4b81a3793a
Lyndon Prado <lyndonprado1@gmail.com>
2020-02-25 13:58:18 +1100

    Installed jquery ui, sortable html5 library, and css calls

--- code/devcamp-tutorials/DevcampPortfolio/app/javascript/packs/application.js
+++ code/devcamp-tutorials/DevcampPortfolio/app/javascript/packs/application.js
@@ -2,11 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+// alert("hello from application.js")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../../javascript/custom/index.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,3 +19,9 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jqeury_ujs
+//= require jquery-ui
+//= require ../../javascript/html.sortable
+//= require turbolinks
+//= require_tree .
+//= require_tree ../../javascript/
