ac186de15bd2212fa97f4fbbb2abc68f38c669df
Lyndon Prado <lyndonprado1@gmail.com>
2020-02-27 16:19:29 +1100

    Updated file upload forms and refactored placeholder functionality into view

--- code/devcamp-tutorials/DevcampPortfolio/app/helpers/portfolios_helper.rb
+++ code/devcamp-tutorials/DevcampPortfolio/app/helpers/portfolios_helper.rb
@@ -1,2 +1,19 @@
 module PortfoliosHelper
+	def image_generator(height:, width:)
+		"http://placehold.it/#{height}x#{width}"
+	end
+	def portfolio_img img, type
+			if (img) && (img.main_image? || img.thumb_image?)
+				img
+			elsif type == 'thumb'
+				image_generator(height: '350', width: '200')
+			else
+				image_generator(height: '600', width: '400')
+			end		
+	end
 end
+# self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
+# 		self.thumb_image ||= Placeholder.image_generator(height: '350',width: '200')
+
+
+	
