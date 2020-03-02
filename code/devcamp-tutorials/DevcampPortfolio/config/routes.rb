b54e20e648231054495c5f873ce027725dd7e392
Lyndon Prado <lyndonprado1@gmail.com>
2020-03-02 16:49:29 +1100

    Created a comment broadcast, configured the websocket route, and updated the comment model with a callback

--- code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
+++ code/devcamp-tutorials/DevcampPortfolio/config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   end
   #get 'portfolios/index' => 'portfolios#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
   root to: 'pages#home'
 end
 
