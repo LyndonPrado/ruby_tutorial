192dcc91ad38f0ba82b97d5d3a124191ad3abadb
Lyndon Prado <lyndonprado1@gmail.com>
2020-03-03 16:48:21 +1100

    Implemented markdown for blog posts

--- code/devcamp-tutorials/DevcampPortfolio/app/helpers/blogs_helper.rb
+++ code/devcamp-tutorials/DevcampPortfolio/app/helpers/blogs_helper.rb
@@ -2,4 +2,26 @@ module BlogsHelper
 	def gravatar_helper user
 		image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
 	end
+
+	class CodeRayify < Redcarpet::Render::HTML
+		def block_code(code, language)
+			CodeRay.scan(code, language || :text || :html).div
+		end
+	end
+
+	def markdown(text)
+		# binding.pry
+		coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+		# binding.pry
+
+		options = {
+			fenced_code_blocks: true,
+			no_intra_emphasis: true,
+			autolink: true,
+			lax_html_blocks:true
+		}
+
+		markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+		markdown_to_html.render(text).html_safe
+	end
 end
